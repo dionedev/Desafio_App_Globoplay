e8cd2f78d610f3571110b692df6593b77c259f4c
Dione Santos <dionesantos@Diones-MacBook-Pro.local>
2024-08-28 18:39:19 -0300

    refactor: componente carousel

--- lib/ui/components/carousel.dart
+++ lib/ui/components/carousel.dart
@@ -1,15 +1,15 @@
-import 'package:app_movie/domain/entity/movie.dart';
 import 'package:flutter/material.dart';
 
 class Carousel extends StatefulWidget {
   final String carouselTitle;
-  final List<Movie> movies;
+  final List<String> items;
+
   final void Function() onNavigate;
 
   const Carousel({
     super.key,
     required this.carouselTitle,
-    required this.movies,
+    required this.items,
     required this.onNavigate,
   });
 
@@ -17,34 +17,37 @@ class Carousel extends StatefulWidget {
   State<Carousel> createState() => _CarouselState();
 }
 
-class _CarouselState extends State<Carousel> {
+class _CarouselState<T> extends State<Carousel> {
   @override
   Widget build(BuildContext context) {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
-        Text(
-          widget.carouselTitle,
-          style: const TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
+        Padding(
+          padding: const EdgeInsets.only(left: 8),
+          child: Text(
+            widget.carouselTitle,
+            style: const TextStyle(
+              fontSize: 28,
+              fontWeight: FontWeight.bold,
+              color: Color.fromRGBO(246, 246, 246, 1),
+            ),
+          ),
         ),
         Container(
           margin: const EdgeInsets.only(top: 20),
           height: 250,
           child: ListView.builder(
             scrollDirection: Axis.horizontal,
-            itemCount: widget.movies.length,
-            itemBuilder: (buildContext, int index) {
-              final movie = widget.movies[index];
+            itemCount: widget.items.length,
+            itemBuilder: (buildContext, index) {
+              final item = widget.items[index];
               return GestureDetector(
                 onTap: widget.onNavigate,
                 child: Container(
                   width: 150,
                   margin: const EdgeInsets.symmetric(horizontal: 8),
-                  child: Image.network(
-                    movie.fullImageUrl,
-                    fit: BoxFit.cover,
-                    width: double.infinity,
-                  ),
+                  child: Image.network(item),
                 ),
               );
             },
